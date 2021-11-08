f2a75cda33516a43d68447d3c908433681714359
davilecm <davidbasket1@gmail.com>
2021-11-08 17:48:39 +0100

    Ejercicio1 Spring

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+
+
+        Saludo hola = (Saludo) context.getBean("saludo");
+
+        String texto = hola.holaMundo();
+        System.out.println(texto);
+
+
+    }
+}
