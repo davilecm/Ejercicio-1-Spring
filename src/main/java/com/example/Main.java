d412666a320dec7a641395d1808a3cdfc8fc3374
davilecm <davidbasket1@gmail.com>
2021-11-08 20:42:03 +0100

    Ejercicio2 Spring

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,22 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/*
+
+Ejercicio 2
+
+Crear la clase NotificationService, con un método que imprima un saludo.
+
+Crear una clase UserService que tenga un atributo de clase NotificationService.
+
+Utilizar la anotación @Component en cada clase.
+
+Habilitar el escaneo de clases en el archivo beans.xml
+
+Desde el método main, probar a obtener el bean UserService y ejecutar el método imprimirSaludo que tiene asociado el atributo de tipo NotificationService.
+De forma similar a la realizada en clase.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,12 +27,8 @@ public class Main {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
 
-
-        Saludo hola = (Saludo) context.getBean("saludo");
-
-        String texto = hola.holaMundo();
-        System.out.println(texto);
-
+        UserService usuario = (UserService) context.getBean(UserService.class);
+        System.out.println(usuario.saludo.holaMundo());
 
     }
 }
--- src/main/java/com/example/Saludo.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Saludo {
-
-    public String holaMundo(){
-
-        return "Hola Mundo";
-    }
-}
